040389e5fcbde14dabd607b93f96908fdf6c6ea5
SERGEYBOBKOV <sergey_bobkov@inbox.ru>
2024-05-26 13:03:07 +0300

    Weather app

---
+++ local-weather/src/components/GetLocation.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+
+import {onMounted, ref} from "vue";
+import type {Ref} from "vue";
+import WeatherReport from "@/components/WeatherReport.vue";
+
+type Geolocation = {
+  latitude: number;
+  longitude: number;
+};
+
+const coords: Ref<Geolocation | undefined> = ref();
+const geolocationBlockedByUser: Ref<boolean> = ref(false);
+
+const getGeolocation = async (): Promise<void> => {
+  navigator?.geolocation?.getCurrentPosition(
+      async (position: { coords: Geolocation }) => {
+        coords.value = position.coords;
+      },
+      (error: { message: string }) => {
+        geolocationBlockedByUser.value = true;
+        console.error(error.message);
+      }
+  );
+};
+
+onMounted(async () => {
+  await getGeolocation();
+})
+
+</script>
+
+<template>
+  <WeatherReport v-if="coords && !geolocationBlockedByUser" :coords="coords" />
+  <div v-if="geolocationBlockedByUser">User denied access</div>
+</template>
---
+++ local-weather/src/components/WeatherReport.vue
@@ -0,0 +1,77 @@
+<script setup lang="ts">
+import {onMounted, ref, type Ref} from "vue";
+
+type WeatherData = {
+  location: {
+    localtime: Date;
+    name: string;
+    region: string;
+  };
+  current: {
+    temp_c: number;
+    temp_f: number;
+    precip_mm: number;
+    condition: {
+      text: string;
+      icon: string;
+    };
+    wind_degree: number;
+    wind_kph: number;
+    wind_mph: number;
+  };
+};
+type Coords = {
+  latitude: number;
+  longitude: number
+}
+
+interface Props {
+  coords: Coords;
+}
+const props = defineProps<Props>();
+
+const data: Ref<WeatherData | undefined> = ref();
+
+// https://www.weatherapi.com/docs/#intro-request
+const fetchWeather = async (coords: Coords): Promise<WeatherData> => {
+
+  const { latitude, longitude } = coords;
+
+  const q = `${latitude},${longitude}`;
+  const res = await fetch(
+      `https://api.weatherapi.com/v1/current.json?key=${
+          import.meta.env.VITE_APP_WEATHER_API_KEY
+      }&q=${q}&lang=nl`
+  );
+  const data = await res && res.json();
+  return data;
+};
+
+onMounted(async () => {
+  const { latitude, longitude } = props.coords;
+  data.value = await fetchWeather({latitude, longitude});
+})
+
+</script>
+
+<template>
+  <article
+      v-if="data && data.current"
+      class="max-w-md w-96 rounded-lg shadow-lg p-4 flex bg-white
+text-black"
+  >
+    <div class="basis-1/4 text-left">
+      <img :src="data.current.condition.icon" class="h-16 w-16" />
+    </div>
+    <div class="basis-3/4 text-left">
+      <h1 class="text-3xl font-bold">
+        {{ data.current.condition.text }}
+        <span class="text-2xl block">{{ data.current.temp_c
+          }}&#8451;</span>
+      </h1>
+      <p>{{ data.location.name }} {{ data.location.region }}</p>
+      <p>Precipitation: {{ data.current.precip_mm }}mm</p>
+    </div>
+  </article>
+  <div v-else>Loading...</div>
+</template>
